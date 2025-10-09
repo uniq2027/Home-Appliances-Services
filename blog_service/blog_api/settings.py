85d2458dc5ff1beb4dd2ea1a90795435bc83febe
Poovarasan99830 <kingpoovarasan49@gmail.com>
2025-10-10 00:12:35 +0530

    changed..!

--- blog_service/blog_api/settings.py
+++ blog_service/blog_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
+    'rest_framework',
+    # your apps
+    'blog',  # <-- add this
 ]
 
 MIDDLEWARE = [
